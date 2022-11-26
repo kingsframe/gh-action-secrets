5ef461d8f8b93f5fb5e47ab95d92f81d37932b06
kai <kaizhangwen@gmail.com>
2022-11-26 09:05:13 -0800

    add upgrade workflow

--- programs/gh-action-scrects/src/lib.rs
+++ programs/gh-action-scrects/src/lib.rs
@@ -1,11 +1,11 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6zdDmuQg36ytdxeXYG6zoYdiiWVPXqao3Fvp2BbfV7o");
 
 #[program]
 pub mod gh_action_scrects {
     use super::*;
-
+    // This is upgrade test 1
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
